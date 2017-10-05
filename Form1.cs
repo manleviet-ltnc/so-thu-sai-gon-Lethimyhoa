34bdbde2a9844a72af8f10ce458ba7341042be83
Lethimyhoa <32034028+Lethimyhoa@users.noreply.github.com>
2017-10-05 16:41:55 +0700

    sở thú sài gòn
    xin chào

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoThu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            lstDanhSachThu.Items.Add(lstThuMoi.SelectedItem);
+        }
+        private void ListBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lb = (ListBox)sender;
+            int index = lb.IndexFromPoint(e.X, e.Y);
+            if (index != -1)
+                lb.DoDragDrop(lb.Items[index].ToString(),
+                              DragDropEffects.Copy);
+
+        }
+    }
+}
